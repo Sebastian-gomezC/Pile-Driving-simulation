083e7a047b41b0ee510483773dd83a7270f51989
Sebastian-gomezC <jsgomezca@unal.edu.co>
2022-03-28 00:03:02 -0500

    modificacion a la formulacion inestable

--- ASSEMBLY_PDE.py
+++ ASSEMBLY_PDE.py
@@ -65,8 +65,8 @@ Save StrCat(StrPrefix(General.FileName), ".msh");
 """
 test_geo="""
 SetFactory("OpenCASCADE");
-ancho = 0.02 ;
-prof =-0.01;
+ancho = 4;
+prof =-1;
 
 Rectangle(1) = {0, 0, 0, ancho, prof, 0};
 Physical Curve("disp",1) = {4,2};
@@ -75,6 +75,7 @@ Physical Curve("far",5) = {3};
 Physical Surface("soil1",1)={1};
 
 Mesh 2 ;
+RefineMesh;
 RefineMesh;
 Mesh.MshFileVersion = 2.2;
 Save StrCat(StrPrefix(General.FileName), ".msh");
@@ -97,7 +98,7 @@ subd= MeshFunction("size_t",mesh,"%s.mesh/"%(nombre)+nombre+"_physical_region.xm
 vtkfile_u = File('%s.results3/u.pvd' % (nombre))
 vtkfile_fs = File('%s.results3/Mohr-Coulomb_Fs.pvd' % (nombre))
 vtkfile_p = File('%s.results3/Pressure.pvd' % (nombre))
-vtkfile_h = File('%s.results3/pressure_head.pvd' % (nombre))
+vtkfile_flow = File('%s.results3/flow.pvd' % (nombre))
 
 #material 
 
@@ -153,7 +154,7 @@ def epsilon(u):
 #esfuerzo 
 def sigma(u):
     return lmbda*div(u)*Identity(d) + 2*mu*epsilon(u)
-ele_p  = FiniteElement("DG",  mesh.ufl_cell(), 1) # pressure
+ele_p  = FiniteElement("CG",  mesh.ufl_cell(), 1) # pressure
 ele_u  = VectorElement("CG",  mesh.ufl_cell(), 2) # solid displacement
 W = MixedElement([ele_p, ele_u])
 W = FunctionSpace(mesh, W)
@@ -162,25 +163,25 @@ V = TestFunction(W)
 Z= FunctionSpace(mesh, 'CG', 1)
 Z_v = VectorFunctionSpace(mesh, 'CG', 1)
 TS = TensorFunctionSpace(mesh, "CG", 1)
-X_n = Function(W)
+
 
 p, u = split(U)
 q, v = split(V)
-p_n, u_n = split(X_n)
+
 
 
 steps =5000
 n=FacetNormal(mesh)#vector normal 
 t=0 # tiempo inicial
 Ti=1#tiempo total
-delta= (Ti-t)/steps
+delta= 2.04E-6
 dt=Constant((delta))
-e=2717000 #modulo elasticidad de prueba 
+e=1#modulo elasticidad de prueba 
 
 E=e#K(subd,1729000,2717000,334000,1252000,3286000) #modulo elasticidad
 theta =18.94#K(subd,18.94,20.61,23.27,20.53,21.84) #angulos friccion interna
 C=15530#K(subd,15530,10350,18650,18400,14000) #cohesion
-nu=Constant(0.35)#coeficiente de poisson  
+nu=Constant(0.25)#coeficiente de poisson  
 mu = E/2/(1+nu)#coeficientes de Lame
 rho=Constant((8000)) #densidad
 lmbda = E*nu/((1+nu)*(1-2*nu))#coeficientes de Lame
@@ -189,7 +190,7 @@ d = u.geometric_dimension()
 f = Constant((0, 0))
 lam=Constant((0))
 ds = Measure('ds', domain=mesh, subdomain_data=contorno)
-T=Constant((0,0))
+
 
 def h(p):
     x=SpatialCoordinate(mesh)
@@ -225,27 +226,28 @@ K5=Constant(((1E-7,0),(0,1E-7)))
 K=Constant(((1,0),(0,1)))#KM(subd,K1,K2,K3,K4,K5)
 H=Expression(('-gam*x[1]'),gam=gam,degree=1)
 
-bp=DirichletBC(W.sub(0),Constant((0)),contorno,2)
-bp1=DirichletBC(W.sub(0),Constant((0)),contorno,5)
 bp2=DirichletBC(W.sub(0),Constant((0)),contorno,1)
 gamma=Constant((1))#biotcoef
 r=0.45
-bc1 = DirichletBC(W.sub(1).sub(0), Constant((0)),contorno,1)
+bc1 = DirichletBC(W.sub(1).sub(0),Constant((0)),contorno,2)
 bc2 = DirichletBC(W.sub(1), Constant((0,0)),contorno,5)
 flo=Constant((0))
-s_coef=1.2*0.5E-9-(gamma-1.2)*0.4
-X_n=Expression(('-4000000*(x[1]+0.005)*(x[1]+0.005)+100','0','0'), gam=gam,degree=3)
-X_n=interpolate(X_n, W)
+s_coef=0.5
+#X_n=Expression(('0','0','0'), gam=gam,degree=3)
+#X_n=interpolate(X_n, W)
+X_n = Function(W)
 p_n, u_n = split(X_n)
 
+T=Expression(('0','0.1'),t=t,degree=2)
+
 F1 = inner(sigma(u), epsilon(v))*dx \
-    - inner(f, v)*dx -\
+    - inner(f, v)*dx +\
     inner(T, v)*ds(subdomain_id=2, domain=mesh, subdomain_data=contorno)\
     - gamma*p*nabla_div(v)*dx 
     
-F2 = dt*inner(nabla_grad(q), K*nabla_grad(p))*dx -\
-    gamma*(nabla_div(u)-nabla_div(u_n))*q*dx -s_coef*(p-p_n)*q*dx\
-        #-dt*flo*q*ds(subdomain_id=2,domain=mesh, subdomain_data=contorno) \
+F2 = +dt*inner(nabla_grad(q), K*nabla_grad(p))*dx -\
+    gamma*(nabla_div(u)-nabla_div(u_n))*q*dx -(p-p_n)*q*dx \
+        -dt*flo*q*ds(subdomain_id=5,domain=mesh, subdomain_data=contorno)  -dt*flo*q*ds(subdomain_id=2,domain=mesh, subdomain_data=contorno) 
 
 
 L_momentum =lhs(F1)
@@ -257,16 +259,24 @@ L= L_mass+L_momentum
 R= R_mass +R_momentum
 X = Function(W)
 
-
-p_n=interpolate(H,Z)
 for pot in range(steps):
-    bcs=[bp,bp1,bc2]
+    bcs=[bp2,bc2,bc1]
+    
+
+
+    #A=assemble(L)
+    #b=assemble(R)
+    #[bc.apply(A) for bc in bcs]
+    #[bc.apply(b) for bc in bcs]
+
+    #solve(A, X.vector(), b)
+    solve(L==R,X,bcs)
+    X_n.assign(X)
     p_n, u_n = split(X_n)
     u_=project(u_n,Z_v)
     p_=project(p_n,Z)
-
     if pot % 10== 0:
-
+        
         s = sigma(u_)
         
         cauchy=project(s,TS)
@@ -279,20 +289,15 @@ for pot in range(steps):
         fail = envFalla(o1, o2,theta,C)
         fs = fail/tm
         fs=project(fs,Z)
+        flow=-K*grad(p_)
+        flow=project(flow,Z_v)
         fs.rename(" Mohr-Coulomb Fs", "Mohr-Coulomb Fs") ;vtkfile_fs << fs
         u_.rename("displacement", "displacement") ;vtkfile_u << u_
+        flow.rename("flow", "flow") ;vtkfile_flow << flow
         p_.rename("pressure", "pressure"); vtkfile_p << p_
-    #A=assemble(L)
-    #b=assemble(R)
-    #[bc.apply(A) for bc in bcs]
-    #[bc.apply(b) for bc in bcs]
-
-    #solve(A, X.vector(), b,'lu')
-    solve(L==R,X,bcs)
-    X_n.assign(X)
-
     print('u max:', u_.vector().get_local().max(),
               'step', pot, 'of', steps,'time:',t)
     print('p max:', p_.vector().get_local().max())
     print('p min:', p_.vector().get_local().min())
+    
     t=t+delta
--- pile_install.mesh/pile_install.geo
+++ pile_install.mesh/pile_install.geo
@@ -1,7 +1,7 @@
 
 SetFactory("OpenCASCADE");
-ancho = 0.02 ;
-prof =-0.01;
+ancho = 4;
+prof =-1;
 
 Rectangle(1) = {0, 0, 0, ancho, prof, 0};
 Physical Curve("disp",1) = {4,2};
@@ -10,6 +10,7 @@ Physical Curve("far",5) = {3};
 Physical Surface("soil1",1)={1};
 
 Mesh 2 ;
+RefineMesh;
 RefineMesh;
 Mesh.MshFileVersion = 2.2;
 Save StrCat(StrPrefix(General.FileName), ".msh");
