c5dce8e2cecd55507f36748c9a2e23cfc64a7a27
Sebastian-gomezC <jsgomezca@unal.edu.co>
2023-04-29 18:06:48 -0500

    correccion de errores en el caso de consolidación multi dominio

--- ASSEMBLY_PDE_Terzaghi_multi_soils.py
+++ ASSEMBLY_PDE_Terzaghi_multi_soils.py
@@ -34,10 +34,10 @@ def create_mesh(mesh, cell_type, prune_z=False):
             out_mesh.prune_z_0()
         return out_mesh
 print('creando malla..')
-L=-3
+h2=3
+h1=1.5
 H=1.5
-mesh=RectangleMesh(Point(-H/2, 0.0), Point(H/2,L), int(H*10), int(-L*20),"crossed")
-h1=-1
+mesh=RectangleMesh(Point(-H/2, 0.0), Point(H/2,-(h1+h2)), int(H*10), int((h1+h2)*10),"crossed")
 #geo = mshr.Rectangle(Point(-0.25, 0.0), Point(0.25,-1))
 #mesh = mshr.generate_mesh(geo,25)
 # cell_markers =  MeshFunction("bool", mesh,mesh.topology().dim())
@@ -49,13 +49,13 @@ h1=-1
 # mesh = refine(mesh, cell_markers)
 
 contorno = MeshFunction("size_t", mesh, mesh.topology().dim()-1)
-tol =1E-3
+tol =1E-6
 class load(SubDomain):
         def inside(self, x, on_boundary):
             return on_boundary and abs(x[1])< tol 
 class bound(SubDomain):
         def inside(self, x, on_boundary):
-            return on_boundary and abs(x[1]-L)< tol 
+            return on_boundary and( x[1]<= -(h1+h2))
 class walls(SubDomain):
         def inside(self, x, on_boundary):
             return on_boundary and ((abs(x[0]+H/2)< tol) or (abs(x[0]-H/2)< tol) )
@@ -66,10 +66,10 @@ walls().mark(contorno, 1)
 subd = MeshFunction("size_t", mesh, mesh.topology().dim())
 class H1(SubDomain):
     def inside(self,x,on_boundary):
-        return x[1]>=h1 
+        return x[1]>=-1.5 
 class H2(SubDomain):
     def inside(self,x,on_boundary):
-        return (x[1]-h1)<tol
+        return (x[1]+1.5)<tol
 
 H2().mark(subd,2)
 H1().mark(subd,1)
@@ -115,7 +115,7 @@ def epsilon(u):
     
 #esfuerzo 
 def sigma(u):
-    return lmbda*div(u)*Identity(d) + 2*mu*epsilon(u)
+    return lmbda*div(u)*Identity(d)+2*mu*epsilon(u)
 ele_p  = FiniteElement("P",mesh.ufl_cell(), 1) # pressure
 ele_u  = VectorElement("P",mesh.ufl_cell(), 1) # solid displacement
 W = MixedElement([ele_p, ele_u])
@@ -133,24 +133,24 @@ q, v = split(V)
 steps =1000
 
 t=0 # tiempo inicial
-Ti=10#tiempo total
+Ti=10800#tiempo total
 delta= Ti/steps
 dt=Constant((delta))
 # B_s=1E-11
-B_m=0.004E-5 #compresibilidad modulo bulk moduls
-B_f=4.4E-5
+# B_m=1E-10
+B_f=4.4E-10
 Poro=0.3
 nu=0.3#coeficiente de poisson  
 flo=Constant((0,0))
-#E=50000000# #modulo elasticidad
-#B_m=(E/(3*(1-2*nu)))**(-1)
+E=50000000# #modulo elasticidad
+B_m=(E/(3*(1-2*nu)))**(-1)
 B_s=B_m/10
 alpha=(1-B_s/B_m) #biotcoef
 s_coef=(alpha-Poro)*B_s +Poro*B_f
 theta =18.94#K(subd,18.94,20.61,23.27,20.53,21.84) #angulos friccion interna
 C=15530#K(subd,15530,10350,18650,18400,14000) #cohesion
 
-E=B_m**(-1)*3*(1-2*nu)#modulo elasticidad 
+#E=B_m**(-1)*3*(1-2*nu)#modulo elasticidad 
 print('modulo elasticidad ',E)
 mu = E/(2*(1+nu))#coeficientes de Lame
 lmbda = E*nu/((1+nu)*(1-2*nu))#coeficientes de Lame
@@ -178,11 +178,22 @@ def sigma_3(T):
     return -b/2 - sqrt(b**2-4*c)/2
 
 n=FacetNormal(mesh)#vector normal      
-k=1.15E-5
-k1=Constant(((1.15E-5,0),(0,1.15E-5)))
-k2=Constant(((1.15E-6,0),(0,1.15E-6)))
-K=K(subd,1.15E-5,1.15E-6)
 
+k1=1.15E-10
+k2=1.15E-11
+mv=1/((1/B_m)+((4/3)*mu))
+m1 = 1/B_m
+m2 = 1/B_m #compressibilities
+gama =10 #unidades KN/m3
+c1 = k1/(alpha**2*mv+s_coef)
+c2 = k2/(alpha**2*mv+s_coef)
+t1 = h1 ** 2 /c1 
+t2 = h2 ** 2 /c2 
+Alpha = ((k2*m2)**(1/2))/((k1*m1)**(1/2))
+Beta = (t1/t2) ** (1/2)
+rango = 100
+cv = c2
+K=K(subd,k1,k2)
 bp1=DirichletBC(W.sub(0),Constant((0)),contorno,2)
 
 
@@ -245,30 +256,110 @@ R_mass=rhs(F2)
 L=L_momentum+L_mass
 R=R_momentum+R_mass
 snaps=100
-mv=1/((1/B_m)+((4/3)*mu))
-cv=k/(alpha**2*mv+s_coef)
-p0= ((alpha*mv)/(alpha**2*mv+s_coef))*Po
-def p_analitico(time,snaps,cv,p0):
-    z=0
-    for n in range(snaps):
-        for i in range(100):
-                if i==0:
-                    p=0
-                else:
-                    c=math.sin((2*i-1)*math.pi*(z)/(2))
-                    b=math.exp(-(((2*i-1)*math.pi)/(2*1))**2*cv*time)
-                    d=((1)/(2*i-1))
-                    p=p+d*c*b
-        p=4/math.pi*(p)
-        if n==0:
-            a=np.array([[p,z]])
+
+
+
+p0= Po
+#def p_analitico(time,snaps,cv,p0):
+#    for n in range(snaps):
+#        for i in range(100):
+#                if i==0:
+#                    p=0
+#                else:
+#                    c=math.sin((2*i-1)*math.pi*(z)/(2))
+#                    b=math.exp(-(((2*i-1)*math.pi)/(2*1))**2*cv*time)
+#                    d=((1)/(2*i-1))
+#                    p=p+d*c*b
+#        p=4/math.pi*(p)
+#        if n==0:
+#            a=np.array([[p,z]])
+#        else:
+#            a= np.append(a,np.array([[p,z]]),axis=0)
+#        z=z+1/snaps
+#    return a
+#Función para calcular los ceros de la función del caso
+
+def roots (n,f):
+    zeros = []
+    e=1E-4
+    epsilonn=1E-6
+    x=0
+    k=0
+    
+    while k<n:
+        if x == 0:
+            y_n = f(x)
+            x +=e
+            continue
         else:
-            a= np.append(a,np.array([[p,z]]),axis=0)
-        z=z+1/snaps
+            y = f(x)
+            if np.sign(y) != np.sign(y_n):
+                a=x-e
+                b=x
+                error=1
+                while error>epsilonn:
+                    m=(a+b)/2
+                    error = (b-a)/2
+                    if f(a)*f(m)>0:
+                        a=m
+                        b=b
+                    elif f(a)*f(m)<0:
+                        a=a
+                        b=m
+                    else :
+                        zeros.append(m)
+                        k+=1
+                        x=b
+                        break 
+                    if error<epsilonn:
+                        zeros.append(m)
+                        k+=1
+                        x=b
+        y_n=f(x)
+        x+=e
+    return zeros
+
+def Q1(x):
+    return -Alpha*np.sin(Beta*x)*np.sin(x) + np.cos(Beta*x)*np.cos(x)
+zeros=roots(rango,Q1)
+#Determinación de p
+def p_analitico(time,snaps,h1,h2,Alpha,Beta):
+    z=h1
+    for n in range(snaps): 
+       if z>0:
+          for i in range (rango):
+            if i==0:
+              p=0
+            else:
+              xi = zeros[i-1]
+              l = math.cos(xi)*math.cos(Beta*xi*(z/h1)) - Alpha* math.sin(xi)* math.sin(Beta*xi*(z/h1)) 
+              m = (1 + Alpha*Beta)*math.cos(Beta*xi)*math.sin(xi) + (Alpha+Beta)* math.sin(Beta*xi)*math.cos(xi)
+              b = math.exp((-xi**2)*time/t2) / xi
+              pa = (l / m ) * b
+              p += 2 * pa
+       elif z<0:
+           for i in range (rango): 
+             if i==0:
+                p=0
+             else:
+              xi = zeros[i-1]
+              l = math.cos(xi)*math.cos(xi*(z/h2)) - math.sin(xi)* math.sin(xi*(z/h2)) 
+              m = (1 + Alpha*Beta)*math.cos(Beta*xi)*math.sin(xi) + (Alpha+Beta)* math.sin(Beta*xi)*math.cos(xi)
+              b = math.exp((-xi**2)*time/t2) / xi
+              pa = (l / m ) * b
+              p += 2 * pa
+       if n==0:
+            a=np.array([[p,-(z+h2)/(h1+h2)+1]])
+       else:
+            a= np.append(a,np.array([[p,-(z+h2)/(h1+h2)+1]]),axis=0)
+       z=z-(h1+h2)/snaps
     return a
+
+
+
 def u_analitico(time,snaps,cv,p0):
     c=-mv*Po*1
-    for i in range(100):
+    for i in range(rango):
             if i==0:
                 p=0
             else:
@@ -285,8 +376,6 @@ f.set_figwidth(10)
 f.set_figheight(10)
 L2=[]
 uplot=[]
-u_f=mv*Po
-print('u final',u_f)
 ig, ax = plt.subplots()
 dtdot=(cv/1**2)*delta
 for pot in range(steps):
@@ -329,21 +418,20 @@ for pot in range(steps):
         u_.rename("displacement", "displacement") ;vtkfile_u << u_
         flow.rename("flow", "flow") ;vtkfile_flow << flow
         p_.rename("pressure", "pressure"); vtkfile_p << p_
-    uplot.append([(u_analitico(t, snaps, cv, p0)+u_0dot)/(u_f-u_0dot),(u_(0,0)[1]+u_0dot)/(u_f-u_0dot),tdot])
-    print(tdot)
+   # uplot.append([(u_analitico(t, snaps, cv, p0)+u_0dot)/(u_f-u_0dot),(u_(0,0)[1]+u_0dot)/(u_f-u_0dot),tdot])
+   # print(tdot)
     z_=0
     for k in range(snaps):
-        pdot=p_(0.0,z_*3)/p0
+        pdot=p_(0.0,z_*4.5)/p0
         if k ==0:
             results=np.array([[pdot,-z_]])
         else:
             results =np.append(results,np.array([[pdot,-z_]]),axis=0)
         z_-= 1/snaps
-    p_a = p_analitico(t,snaps,cv,p0)
+    p_a = p_analitico(t,snaps,h1,h2,Alpha,Beta)
     L2.append([np.sum((results[:,0]-p_a[:,0])**2),tdot])
-    if near(t,0.01/(cv/1**2),dt/2) or near(t,0.1/(cv/1**2),dt/2) or near(t,0.1/(cv/1**2),dt/2) or near(t,0.5/(cv/1**2),dt/2)or near(t,1/(cv/1**2),dt/2):
-        
-        
+    print(t, " close  to : ",1/(cv/h2**2))
+    if near(t,0.01/(cv/h2**2),dt/2) or near(t,0.1/(cv/h2**2),dt/2) or near(t,0.1/(cv/h2**2),dt/2) or near(t,0.5/(cv/h2**2),dt/2)or near(t,1/(cv/h2**2),dt/2):
         line1, =ax.plot(results[:, 0], results[:, 1], "-",color='red',label='FEM',)
         plt.xlabel("$P*$ ",fontsize=20)
         plt.ylabel("$z*$",fontsize=20)
@@ -352,33 +440,21 @@ for pot in range(steps):
         lines = plt.gca().get_lines()
         l1=lines[-1]
         labelLine(l1,0.4,label=r'$t*=${}'.format(round(tdot,2)),ha='left',va='bottom',align = True)
-        if near(t,0.01/(cv/1**2),dt/2) :
+        if near(t,0.01/(cv/h2**2),dt/2) :
             ax.legend(handler_map={line1: HandlerLine2D(numpoints=4)},loc= 'upper center')
             
-        plt.ylim((0,1.05))
-        plt.xlim((0,1.05))
-        print('error en la norma l2',np.sum((results[:,0]-p_a[:,0])**2 ))
+
+        print('error en la norma l2',np.sqrt(np.sum((results[:,0]-p_a[:,0])**2 )))
         print('u max:',u_.vector().get_local().min(),'step', pot, 'of', steps,'time*:',tdot)
         print('p max:', p_.vector().get_local().max())
         print('p min:', p_.vector().get_local().min())
         
         
-    if near(t,2/(cv/1**2),dt/2):
+    if near(t,1/(cv/h2**2),dt/2):
         break
     
     t += delta
 plt.savefig('resultados_%s/consolidacion_dt%s_grosse_%s.png'%(nombre,round(dtdot,5),scheme),dpi=300)
-plt.close()
-ig, ax = plt.subplots()
-uplot=np.array(uplot)
-line1, =ax.plot(uplot[:,2],-uplot[:,0],"--",color='black',label='Analítica')
-line2,=ax.plot(uplot[:,2],-uplot[:,1], "-",color='red',label='FEM')
-ax.legend(handler_map={line1: HandlerLine2D(numpoints=4)},loc= 'upper left')
-ax.set_xscale('log')
-plt.xlabel("$t*$ ",fontsize=20)
-plt.ylabel("$u*_{z}$",fontsize=20)
-plt.grid(True,color='k',which="both",alpha=0.3, linestyle='-', linewidth=0.5)
-plt.savefig('resultados_%s/disp_dt%s_grosse_%s.png'%(nombre,round(dtdot,5),scheme),dpi=300,)
 plt.close()
 
 L2=np.array(L2)
