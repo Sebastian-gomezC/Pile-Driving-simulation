16276edb898f53cf7eb9bf71111964b6a221e2c6
Sebastian-gomezC <jsgomezca@unal.edu.co>
2025-01-22 11:03:35 -0500

    limpieza del repositorio se añade un folder con el tutorial e instalacion de fenics, se eliminan archivos de viejas versiones y de mecanica nolineal

--- Consolidaciones_de_Terzaghi/simple/jitfailure-dolfin_expression_17352153ec443d70a467b0f69d5d025b/recompile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Execute this file to recompile locally
-c++ -Wall -shared -fPIC -std=c++11 -O3 -fno-math-errno -fno-trapping-math -ffinite-math-only -I/usr/local/include -I/usr/local/slepc-32/include -I/usr/local/petsc-32/include -I/usr/include/mpich -I/usr/include/hdf5/mpich -I/usr/include/eigen3 -I/usr/local/lib/python3.6/dist-packages/ffc/backends/ufc -I/root/.cache/dijitso/include dolfin_expression_17352153ec443d70a467b0f69d5d025b.cpp -L/usr/local/petsc-32/lib -L/usr/local/slepc-32/lib -L/usr/lib/x86_64-linux-gnu/hdf5/mpich -L/usr/local/lib -L/root/.cache/dijitso/lib -Wl,-rpath,/root/.cache/dijitso/lib -lmpich -lmpichcxx -lpetsc -lslepc -lm -ldl -lz -lsz -lhdf5 -lboost_timer -ldolfin -olibdijitso-dolfin_expression_17352153ec443d70a467b0f69d5d025b.so
